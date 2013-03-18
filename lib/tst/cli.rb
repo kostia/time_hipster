822845c74d44d6e65932783c97de1c09f5e9d659
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2013-03-18 17:05:14 +0100

    CLI added

---
+++ lib/tst/cli.rb
@@ -0,0 +1,24 @@
+require 'thor'
+
+require File.expand_path('../application', __FILE__)
+
+module Tst
+  class CLI < Thor
+    desc 'start PROJECT-ID', 'Start time tracking of PROJECT-ID'
+    def start_project(project_id)
+      call(project_id, :start)
+    end
+
+    desc 'stop PROJECT-ID', 'Stop time tracking of PROJECT-ID'
+    def stop_project(project_id)
+      call(project_id, :stop)
+    end
+
+    private
+
+    def call(project_id, action_name)
+      Application.run
+      Application.call("/projects/#{project_id}/#{action_name}")
+    end
+  end
+end
